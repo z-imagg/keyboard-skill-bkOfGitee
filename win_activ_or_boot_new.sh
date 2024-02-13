be24b43a269d1db19407d6358afa58a0844818b7
prgrmz07 <prgrmz07@163.com>
2024-02-13 11:38:43 +0800

    添加 win_activ_or_boot_new.sh

---
+++ win_activ_or_boot_new.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+errMsg="win_activ_or_boot_new.sh: err arg1 required,exit code 23" 
+#若函数参数少于1个，则退出（退出码为23）
+[ $# -lt 1 ] && { echo  $errMsg && return 23 ;}
+
+app_name=$1
+xdotool search --onlyvisible  $win_name windowactivate && echo "已激活 $win_name"
+
+( pidof $app_name && xdotool search --onlyvisible  $app_name windowactivate ) || ( echo "新启动【$app_name】" && $app_name & ) 
