825096949ed3469ae94cc9a95e78cf29f932f132
prgrmz07 <prgrmz07@163.com>
2024-02-14 14:55:32 +0800

    简化快捷键观看文档(使用）

--- sxhkd_xdotool_script/sxhkd_reboot_delay_async.sh
+++ sxhkd_xdotool_script/sxhkd_reboot_delay_async.sh
@@ -4,7 +4,8 @@ set -x
 
 #用kdialog显示一个对本自定义快捷键干净的窗口
 kill $(pidof kdialog)
-grep "##" /app/keyboard-skill/.config/sxhkd/sxhkdrc  | kdialog --textbox -  650 300 &
+grep "##" /app/keyboard-skill/.config/sxhkd/sxhkdrc | sed "s/##//g" | kdialog --textbox -  650 300 &
+#kdialog 文档: https://develop.kde.org/docs/administration/kdialog/#kdialog-dialog-types
 
 { ( sleep 2 &&  sxhkd & ) & } && kill $(pidof sxhkd)
 
