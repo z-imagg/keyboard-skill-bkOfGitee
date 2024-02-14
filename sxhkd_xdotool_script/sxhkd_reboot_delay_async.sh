3d9d4c7604a84299f8f6bbac827ed5ed22294c12
prgrmz07 <prgrmz07@163.com>
2024-02-14 13:49:55 +0800

    kdialog  只显示 快捷键说明

--- sxhkd_xdotool_script/sxhkd_reboot_delay_async.sh
+++ sxhkd_xdotool_script/sxhkd_reboot_delay_async.sh
@@ -4,7 +4,7 @@ set -x
 
 #用kdialog显示一个对本自定义快捷键干净的窗口
 kill $(pidof kdialog)
-kdialog --textbox /app/keyboard-skill/.config/sxhkd/sxhkdrc  800 900 &
+grep "##" /app/keyboard-skill/.config/sxhkd/sxhkdrc  | kdialog --textbox -  650 300 &
 
 { ( sleep 2 &&  sxhkd & ) & } && kill $(pidof sxhkd)
 
