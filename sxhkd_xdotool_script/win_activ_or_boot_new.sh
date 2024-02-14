c85d75e4906f813983eb543246477a8346c5e358
prgrmz07 <prgrmz07@163.com>
2024-02-14 11:35:14 +0800

    命令 gnome-terminal打开的众多窗口 归属到单进程gnome-terminal-server,快捷键变更

--- sxhkd_xdotool_script/win_activ_or_boot_new.sh
+++ sxhkd_xdotool_script/win_activ_or_boot_new.sh
@@ -5,5 +5,6 @@ errMsg="win_activ_or_boot_new.sh: err arg1 required,exit code 23"
 [ $# -lt 1 ] && { echo  $errMsg && return 23 ;}
 
 app_name=$1
-
-( pidof $app_name && xdotool search --onlyvisible  $app_name windowactivate ) || ( echo "新启动【$app_name】" && $app_name & ) 
+set -x
+( pidof $app_name && xdotool search --onlyvisible  $app_name windowactivate ) || { echo "新启动【$app_name】" && $app_name & }
+set +x
