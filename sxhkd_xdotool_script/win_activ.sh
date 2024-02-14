22d22d898c0dc636b27bdab127f6488f10d42bae
prgrmz07 <prgrmz07@163.com>
2024-02-14 11:38:19 +0800

    set +x 可以塞入多命令中间合适的位置 以阻止echo被set -x显示两次

--- sxhkd_xdotool_script/win_activ.sh
+++ sxhkd_xdotool_script/win_activ.sh
@@ -4,5 +4,6 @@ errMsg="win_activ.sh: err arg1 required,exit code 23"
 #若函数参数少于1个，则退出（退出码为23）
 [ $# -lt 1 ] && { echo  $errMsg && return 23 ;}
 
+set -x
 win_name=$1
-xdotool search --onlyvisible  $win_name windowactivate && echo "已激活 $win_name"
+xdotool search --onlyvisible  $win_name windowactivate && set +x && echo "已激活 $win_name"
