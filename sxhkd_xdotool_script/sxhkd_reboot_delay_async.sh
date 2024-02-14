a4c5449ff50c3cc7e0e3d28504ea705250ffc5ff
prgrmz07 <prgrmz07@163.com>
2024-02-14 13:47:41 +0800

    kdialog 放进重启sxhkd中

--- sxhkd_xdotool_script/sxhkd_reboot_delay_async.sh
+++ sxhkd_xdotool_script/sxhkd_reboot_delay_async.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 set -x
+
+#用kdialog显示一个对本自定义快捷键干净的窗口
+kill $(pidof kdialog)
+kdialog --textbox /app/keyboard-skill/.config/sxhkd/sxhkdrc  800 900 &
+
 { ( sleep 2 &&  sxhkd & ) & } && kill $(pidof sxhkd)
 
 set +x
-{ ( sleep 2.3 && echo "sxhkd's PID: 【$(pidof sxhkd)】" ) & }
+{ ( sleep 2.3 && echo -e "sxhkd's PID: 【$(pidof sxhkd)】; ok \n" ) & }
