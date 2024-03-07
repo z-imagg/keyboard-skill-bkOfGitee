2cf3695a734951490a1e3ca444a2419002409a67
prgrmz07 <prgrmz07@163.com>
2024-03-07 14:16:29 +0800

    添加 sxhkd_xdotool_script/min_all_win_except_cur.sh

---
+++ sxhkd_xdotool_script/min_all_win_except_cur.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+#最小化非当前窗口
+
+# 1.保存当前窗口id
+curr_win_id=$(xdotool getactivewindow )
+
+# 2.最小化所有窗口
+wmctrl -k on
+
+# 3.还原当前窗口尺寸
+wmctrl -ir   ${curr_win_id}  -b remove,maximized_vert,maximized_horz
+
