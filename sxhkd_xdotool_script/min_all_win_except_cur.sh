224bd6a0923e7c0487dd31512639a412586f9551
prgrmz07 <prgrmz07@163.com>
2024-03-07 14:31:40 +0800

    更新 sxhkd_xdotool_script/min_all_win_except_cur.sh

--- sxhkd_xdotool_script/min_all_win_except_cur.sh
+++ sxhkd_xdotool_script/min_all_win_except_cur.sh
@@ -8,5 +8,10 @@ curr_win_id=$(xdotool getactivewindow )
 # 2.最小化所有窗口
 wmctrl -k on
 
-# 3.还原当前窗口尺寸
-wmctrl -ir   ${curr_win_id}   -b add,maximized_vert,maximized_horz
+# 3.最大化当前窗口尺寸(估计不需要这一步)
+#wmctrl -ir   ${curr_win_id}   -b add,maximized_vert,maximized_horz
+
+
+# 4. 激活当前窗口
+wmctrl -i -a ${curr_win_id}
+#或#xdotool windowactivate   ${curr_win_id}
