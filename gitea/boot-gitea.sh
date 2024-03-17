fabe1f9f1a80af0bd9aa2332c710bb42be7f94d6
prgrmz07 <prgrmz07@163.com>
2024-03-17 14:12:36 +0800

    更新 gitea/boot-gitea.sh

--- gitea/boot-gitea.sh
+++ gitea/boot-gitea.sh
@@ -5,7 +5,7 @@ usage='su - g -c "bash /home/g/boot-gitea.sh"'
 
 function PrintUsage {
 #在stderr中打印人类可读的提示信息
-echo -e "\e[1;31m『stderr』【命令用法错误，此命令的正确勤快用法『${usage}』已经放入stdout，正确偷懒用法为『\$(bash boot-gitea.sh』】\e[0m" >&2
+echo -e "\e[1;31m『stderr』【命令用法错误，此命令的正确勤快用法『${usage}』已经放入stdout，因此正确偷懒用法为『\$(bash boot-gitea.sh』】\e[0m" >&2
 #在stdout中打印单纯的命令调用语句, 使得 直接 $(bash boot-gitea.sh) 即可正确执行此命令
 echo -n "${usage}"
 }
