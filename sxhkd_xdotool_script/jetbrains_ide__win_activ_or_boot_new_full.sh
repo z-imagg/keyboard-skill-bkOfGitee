83847c18d39704a094653b503dbaacdc9ee5fa50
prgrmz07 <prgrmz07@163.com>
2024-06-09 09:39:33 +0800

    改进

--- sxhkd_xdotool_script/jetbrains_ide__win_activ_or_boot_new_full.sh
+++ sxhkd_xdotool_script/jetbrains_ide__win_activ_or_boot_new_full.sh
@@ -3,31 +3,35 @@
 #[描述] 根据是否有jetbrain_ide clion进程 来启动新clion或激活clion窗口
 #[用法] source /app/keyboard-skill/sxhkd_xdotool_script/jetbrains_ide__win_activ_or_boot_new_full.sh ;  jetbrains_ide__win_activ_or_boot_new_full clion  /app/clion-2022.3.3/bin/clion.sh
 
-errMsg="err, usage: me.sh app_name win_name cmd_full; exit code 25" 
-#若函数参数少于2个，则退出（退出码为25）
-[ $# -lt 2 ] && { echo  $errMsg && return 25 ;}
-
 
 function jetbrains_ide__win_activ_or_boot_new_full(){
 local OK=0
+local ERR=25
+
 local jetbrain_ide_name=$1
 local win_name=$jetbrain_ide_name
 local cmd_full=$2
 
+local errMsg="err, usage: source jetbrains_ide__win_activ_or_boot_new_full.sh  ; example: jetbrains_ide__win_activ_or_boot_new_full clion  /app/clion-2022.3.3/bin/clion.sh; exit code 25" 
+#若函数参数少于2个，则退出（退出码为25）
+[ $# -lt 2 ] && { echo  $errMsg && return $ERR ;}
+
+
+
 source   /app/bash-simplify/jetbrain_java_pid_get.sh
 
-echo "jetbrain_ide_name=【$jetbrain_ide_name】,win_name=【$win_name】,cmd_full=【$cmd_full】"
+echo "[收到入参] jetbrain_ide_name=【$jetbrain_ide_name】,win_name=【$win_name】,cmd_full=【$cmd_full】"
 
 local has_process=false
 jetbrains_ide_process_find $jetbrain_ide_name && jetbrain_ide_jvm_pid=$(jetbrains_ide_process_find $jetbrain_ide_name)  && has_process=true
 
 set -x
 #若已有该进程，则激活窗口即可
-$has_process && xdotool search --onlyvisible  "$win_name" windowactivate && { set +x; return $Ok ;}
+$has_process && xdotool search --onlyvisible  "$win_name" windowactivate && { set +x; return $OK ;}
 
 #否则（尚无该进程） ，则后台启动新启动
 local msg="新启动jetbrain_ide【$cmd_full】"
-( echo "$msg" && $cmd_full & )  &&  { set +x; return $Ok ;}
+( echo "$msg" && $cmd_full & )  &&  { set +x; return $OK ;}
 
 
 #调试语句（给定窗口名，打印窗口id、命令全路径、窗口全名）：
