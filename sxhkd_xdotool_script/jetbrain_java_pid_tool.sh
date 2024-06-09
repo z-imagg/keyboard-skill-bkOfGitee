aca82c6adad2a798c182d4562c2ab6c0cb81c022
prgrmz07 <prgrmz07@163.com>
2024-06-09 08:28:54 +0800

    修复问题 , |while 改为 mapfile 数组 ... for 该数组

--- sxhkd_xdotool_script/jetbrain_java_pid_tool.sh
+++ sxhkd_xdotool_script/jetbrain_java_pid_tool.sh
@@ -26,13 +26,24 @@ local jetbrains_ide_name=$1
 local OK=0
 local NO=1
 local has=false
+
 # pidof java  可以替换 jps
-jps -q | while IFS= read -r pid_j ; do  jetbrains_jvm_pid_is_x $jetbrains_ide_name  $pid_j && echo loop_j_find_clion && has=true; done
+
+local pids=
+mapfile -t pids < <(jps -q)
+
+for pid_j in "${pids[@]}"; do
+    jetbrains_jvm_pid_is_x "$jetbrains_ide_name" "$pid_j" && has=true
+done
+
+
 $has && return $OK
 return $NO
 }
 #函数单元测试
 #jetbrains_ide_process_find clion && echo find_clion
+##find_clion
+#jetbrains_ide_process_find pycharm && echo find_pycharm
 
 #开发用语句:
 #pid=$(/app/jdk8/bin/jps -l | grep idea|cut -d' ' -f1)
